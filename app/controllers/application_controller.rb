34f4c48edda54110488d58c6220a714c081f0bcf
Iguchi-no <iguchipro1029@gmail.com>
2023-03-12 15:03:48 +0900

    0312

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    case resource
+    when Owner
+      user_posts_path
+    when Employee
+      admin_user_posts_path
+    end
+  end
 end
